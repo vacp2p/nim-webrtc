34dd277bc9cfb2e9bc6660c160e0eb35e041ab09
Ludovic Chenut <ludovic@status.im>
2023-04-28 17:06:33 +0200

    Creation of the webrtc connection object

---
+++ webrtc/stun_connection.nim
@@ -0,0 +1,48 @@
+# Nim-WebRTC
+# Copyright (c) 2023 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
+import chronos
+import webrtc_connection, stun
+
+type
+  StunConn* = ref object of WebRTCConn
+    recvData: seq[seq[byte]]
+    recvEvent: AsyncEvent
+    handlesFut: Future[void]
+
+proc handles(self: StunConn) {.async.} =
+  while true: # TODO: while not self.conn.atEof()
+    let msg = await self.conn.read()
+    if Stun.isMessage(msg):
+      let res = Stun.getResponse(msg, self.address)
+      if res.isSome():
+        await self.conn.write(res.get())
+    else:
+      recvData.add(msg)
+      recvEvent.fire()
+
+method init(self: StunConn, conn: WebRTCConn, address: TransportAddress) {.async.} =
+  procCall(WebRTCConn(self).init(conn, address))
+
+  self.recvEvent = newAsyncEvent()
+  self.handlesFut = handles()
+
+method close(self: StunConn) {.async.} =
+  self.handlesFut.cancel() # check before?
+  self.conn.close()
+
+method write(self: StunConn, msg: seq[byte]) {.async.} =
+  await self.conn.write(msg)
+
+method read(self: StunConn): seq[byte] {.async.} =
+  while self.recvData.len() <= 0:
+    self.recvEvent.clear()
+    await self.recvEvent.wait()
+  result = self.recvData[0]
+  self.recvData.delete(0..0)
---
+++ webrtc/udp_connection.nim
@@ -0,0 +1,43 @@
+# Nim-WebRTC
+# Copyright (c) 2023 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
+import chronos
+import webrtc_connection
+
+type
+  UdpConn* = ref object of WebRTCConn
+    udp: DatagramTransport
+    recvData: seq[seq[byte]]
+    recvEvent: AsyncEvent
+
+method init(self: UdpConn, conn: WebRTCConn, address: TransportAddress) {.async.} =
+  procCall(WebRTCConn(self).init(conn, address))
+
+  proc onReceive(udp: DatagramTransport, address: TransportAddress) {.async, gcsafe.} =
+    let msg = udp.getMessage()
+    self.recvData.add(msg)
+    self.recvEvent.fire()
+
+  self.recvEvent = newAsyncEvent()
+  self.udp = newDatagramTransport(onReceive)
+
+method close(self: UdpConn) {.async.} =
+  self.udp.close()
+  if not self.conn.isNil():
+    self.conn.close()
+
+method write(self: UdpConn, msg: seq[byte]) {.async.} =
+  await self.udp.sendTo(self.address, msg)
+
+method read(self: UdpConn): seq[byte] {.async.} =
+  while self.recvData.len() <= 0:
+    self.recvEvent.clear()
+    await self.recvEvent.wait()
+  result = self.recvData[0]
+  self.recvData.delete(0..0)
