a65d905fb8eeca74a120c191dee12d36488051cb
Ludovic Chenut <ludovic@status.im>
2023-08-18 17:20:18 +0200

    remove useless comments

--- examples/sctp_both.nim
+++ examples/sctp_both.nim
@@ -3,7 +3,6 @@ import ../webrtc/sctp as sc
 
 let sctp = Sctp.new(port = 4242)
 proc serv(fut: Future[void]) {.async.} =
-  #let sctp = Sctp.new(port = 4242)
   sctp.startServer(13)
   fut.complete()
   let conn = await sctp.listen()
@@ -18,7 +17,6 @@ proc main() {.async.} =
   let fut = Future[void]()
   asyncSpawn serv(fut)
   await fut
-  #let sctp = Sctp.new(port = 4244)
   let address = TransportAddress(initTAddress("127.0.0.1:4242"))
   let conn = await sctp.connect(address, sctpPort = 13)
   await conn.write("test".toBytes)
