6a355fab364cbeb27910db3daac1007944966f18
Ludovic Chenut <ludovic@status.im>
2024-07-05 13:41:06 +0200

    chore: remove flakyAsyncTest

--- tests/asyncunit.nim
+++ tests/asyncunit.nim
@@ -16,25 +16,3 @@ template asyncSetup*(body: untyped): untyped =
       proc() {.async, gcsafe.} =
         body
     )())
-
-template flakyAsyncTest*(name: string, attempts: int, body: untyped): untyped =
-  test name:
-    var attemptNumber = 0
-    while attemptNumber < attempts:
-      let isLastAttempt = attemptNumber == attempts - 1
-      inc attemptNumber
-      try:
-        waitFor((
-          proc() {.async, gcsafe.} =
-            body
-        )())
-      except Exception as e:
-        if isLastAttempt: raise e
-        else: testStatusIMPL = TestStatus.FAILED
-      finally:
-        if not isLastAttempt:
-          if testStatusIMPL == TestStatus.FAILED:
-            # Retry
-            testStatusIMPL = TestStatus.OK
-          else:
-            break
